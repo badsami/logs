49c7559b9949ac3f3ef43b3be1e7fecf4c81c8bf
Bad-Sam <sami.amara@pm.me>
2023-12-01 22:34:21 +0100

    Updated logs usage example

--- example.c
+++ example.c
@@ -1,5 +1,4 @@
 #include "logs.h"
-#include "num_to_str.h"
 
 __declspec(dllimport) void __stdcall ExitProcess(u32);
 
@@ -7,47 +6,41 @@ s32 _fltused;
 
 void mainCRTStartup()
 {
+  const char* logs_file_name = "logs.txt";
+  
   // Open outputs
   logs_open_console_output();
-  logs_open_file_output("logs.txt");
-  
-  char i_str[2];
-  
-  for (int i = 0; i < 10; i++)
-  {
-    // Enable/disable outputs:
-    // - when i is odd, write to the logs file only
-    // - when i is even, write to logs console only
-    logs_enable_output((i & 0b1) ? LOGS_OUTPUT_FILE : LOGS_OUTPUT_CONSOLE);
-    logs_disable_output((i & 0b1) ? LOGS_OUTPUT_CONSOLE : LOGS_OUTPUT_FILE);
-    
-    // Format i to a string once, then reuse the string
-    u32 i_str_size = s32_to_str(i_str, i);
-    
-    for (int j = 0; j < 100; j++)
-    {
-      // Append content to the logs buffer. Thee size of the logs buffer is
-      // limited: it is a compile time macro constant (LOGS_BUFFER_SIZE)
-      // defaulting to 512, which can be changed througn compiler flags.
-      // No checks are performed to verify if what's appended to the logs
-      // buffer actually fits in.
-      logs_append_str(i_str, i_str_size);
-      logs_append_char('.');
-      logs_append_s32(j);
-      logs_append_char(' ');
-    }
+  logs_open_file_output(logs_file_name);
 
-    // Write the buffered logs to open and enabled outputs
+  u32 bits = 0x3C000000;
+  for (u32 bit_pos = 24; bit_pos > 15; bit_pos--)
+  {
+    bits |= (1 << bit_pos);
+    f32 value = *(f32*)&bits;
+
+    // Format logs
+    logs_append_hex(bits);
+    logs_append_literal(" (");
+    logs_append_u32(bits);
+    logs_append_literal(") as a f32 is ");
+    logs_append_f32(value);
+    logs_append_char('\n');
+
+    // When deemed ready, write buffered logs to enabled outputs
     logs_flush();
   }
 
-  logs_append_char('\n');
+  // Write to specific outputs
+  logs_disable_output(LOGS_OUTPUT_CONSOLE);
+  logs_append_literal("========== Logging session end ==========\n\n");
   logs_flush();
-
+  
   logs_enable_output(LOGS_OUTPUT_CONSOLE);
-  logs_append_literal("\nSee the content of logs.txt in the active directory\n");
+  logs_disable_output(LOGS_OUTPUT_FILE);
+  logs_append_literal("\nLogs written to file ");
+  logs_append_cstr(logs_file_name);
 
-  // Close outputs
+  // Close outputs, implicitly flushing logs buffer to enable outputs
   logs_close_file_output();
   logs_close_console_output();
 
