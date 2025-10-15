0e0b1764f263ed89d6c97a90a5f12464adef61ef
Bad-Sam <sami.amara@protonmail.com>
2025-10-15 22:02:53 +0200

    Replaced counter types from u32 to u64 (with the exception of log_sized_utf16_str()), which generates better code. Improved u32_digit_count(), which is now faster, smaller, and has higher throughput

--- to_str_utilities.c
+++ to_str_utilities.c
@@ -11,7 +11,7 @@ extern u8 _BitScanReverse64(u32* msb_idx, u64 mask);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 //// Intrisics wrapper
-u32 bsr32(u32 num)
+u64 bsr32(u32 num)
 {
   u32 msb_idx;
   _BitScanReverse(&msb_idx, num);
@@ -19,7 +19,7 @@ u32 bsr32(u32 num)
 }
 
 
-u32 bsr64(u64 num)
+u64 bsr64(u64 num)
 {
   u32 msb_idx;
   _BitScanReverse64(&msb_idx, num);
@@ -29,112 +29,97 @@ u32 bsr64(u64 num)
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 //// Numerals count in a number
-u32 u32_digit_count(u32 num)
+u64 u32_digit_count(u32 num)
 {
-  // https://commaok.xyz/post/lookup_tables/
-  static const u64 table[32] =
+  // Similar to https://commaok.xyz/post/lookup_tables/, but:
+  // - lzcnt replaces bsr
+  // - table is reversed
+  // - an extra entry is added for num = 0, so this function returns 1
+  // - the bitwise OR and substraction performed by int_log2() no longer happen
+  static const u64 offsets[33] =
   {
-    4294967296,                                         // (1  << 32)
-    8589934582,  8589934582,  8589934582,               // (2  << 32) - 10
-    12884901788, 12884901788, 12884901788,              // (3  << 32) - 100
-    17179868184, 17179868184, 17179868184,              // (4  << 32) - 1000
-    21474826480, 21474826480, 21474826480, 21474826480, // (5  << 32) - 10000
-    25769703776, 25769703776, 25769703776,              // (6  << 32) - 100000
-    30063771072, 30063771072, 30063771072,              // (7  << 32) - 1000000
-    34349738368, 34349738368, 34349738368, 34349738368, // (8  << 32) - 10000000
-    38554705664, 38554705664, 38554705664,              // (9  << 32) - 100000000
-    41949672960, 41949672960, 41949672960,              // (10 << 32) - 1000000000
-    42949672960, 42949672960                            // (10 << 32)
+    42949672960ull, 42949672960ull,                                 // (10 << 32)
+    41949672960ull, 41949672960ull, 41949672960ull,                 // (10 << 32) - 1000000000
+    38554705664ull, 38554705664ull, 38554705664ull,                 // (9  << 32) - 100000000
+    34349738368ull, 34349738368ull, 34349738368ull, 34349738368ull, // (8  << 32) - 10000000
+    30063771072ull, 30063771072ull, 30063771072ull,                 // (7  << 32) - 1000000
+    25769703776ull, 25769703776ull, 25769703776ull,                 // (6  << 32) - 100000
+    21474826480ull, 21474826480ull, 21474826480ull, 21474826480ull, // (5  << 32) - 10000
+    17179868184ull, 17179868184ull, 17179868184ull,                 // (4  << 32) - 1000
+    12884901788ull, 12884901788ull, 12884901788ull,                 // (3  << 32) - 100
+    8589934582ull,  8589934582ull,  8589934582ull,                  // (2  << 32) - 10
+    4294967296ull,                                                  // (1  << 32)
+    4294967296ull                                                   // (1  << 32)
   };
 
-  u32 msb_idx = bsr32(num | 0b1);
-  return (num + table[msb_idx]) >> 32;
-}
+  u64 lzcnt       = __lzcnt(num);
+  u64 offset      = offsets[lzcnt];
+  u64 digit_count = (num + offset) >> 32;
 
-
-u32 u64_digit_count(u64 num)
-{
-  static const u32 msb_to_max_digit_count[64] =
-  {
-    1, 1, 1,
-    2, 2, 2,
-    3, 3, 3,
-    4, 4, 4, 4,
-    5, 5, 5,
-    6, 6, 6,
-    7, 7, 7, 7,
-    8, 8, 8,
-    9, 9, 9,
-    10, 10, 10, 10,
-    11, 11, 11,
-    12, 12, 12,
-    13, 13, 13, 13,
-    14, 14, 14,
-    15, 15, 15,
-    16, 16, 16, 16,
-    17, 17, 17,
-    18, 18, 18,
-    19, 19, 19, 19,
-    20
-  };
-  
-  static const u64 thresholds[20] =
-  {
-    0ULL,
-    10ULL,
-    100ULL,
-    1000ULL,
-    10000ULL,
-    100000ULL,
-    1000000ULL,
-    10000000ULL,
-    100000000ULL,
-    1000000000ULL,
-    10000000000ULL,
-    100000000000ULL,
-    1000000000000ULL,
-    10000000000000ULL,
-    100000000000000ULL,
-    1000000000000000ULL,
-    10000000000000000ULL,
-    100000000000000000ULL,
-    1000000000000000000ULL,
-    10000000000000000000ULL
-  };
-
-  u32 msb_idx = bsr64(num | 0b1);
-
-  u32 digit_count = msb_to_max_digit_count[msb_idx];
-  digit_count -= (num < thresholds[digit_count - 1u]);
   return digit_count;
 }
 
 
-u32 u32_bit_count(u32 num)
+u64 u64_digit_count(u64 num)
 {
-  return bsr32(num | 0b1) + 1u;
+  // https://lemire.me/blog/2025/01/07/counting-the-digits-of-64-bit-integers/
+  static u64 thresholds[19] =
+  {
+    9ull,
+    99ull,
+    999ull,
+    9999ull,
+    99999ull,
+    999999ull,
+    9999999ull,
+    99999999ull,
+    999999999ull,
+    9999999999ull,
+    99999999999ull,
+    999999999999ull,
+    9999999999999ull,
+    99999999999999ull,
+    999999999999999ull,
+    9999999999999999ull,
+    99999999999999999ull,
+    999999999999999999ull,
+    9999999999999999999ull
+  };
+  u64 msb_idx       = bsr64(num | 0b1);
+  u64 threshold_idx = (19ull * msb_idx) >> 6;
+  u64 threshold     = thresholds[threshold_idx];
+  u64 is_greater    = num > threshold;
+  u64 digit_count   = threshold_idx + is_greater + 1ull;
+
+  return digit_count;
 }
 
 
-u32 u64_bit_count(u64 num)
+u64 u32_bit_count(u32 num)
 {
-  return bsr64(num | 0b1) + 1u;
+  return bsr32(num | 0b1) + 1ull;
 }
 
 
-u32 u32_nibble_count(u32 num)
+u64 u64_bit_count(u64 num)
 {
-  u32 msb_idx      = bsr32(num | 0b1);
-  u32 nibble_count = 1u + (msb_idx >> 2);
+  return bsr64(num | 0b1) + 1ull;
+}
+
+
+u64 u32_nibble_count(u32 num)
+{
+  u64 msb_idx      = bsr32(num | 0b1);
+  u64 nibble_count = 1ull + (msb_idx >> 2);
 
   return nibble_count;
 }
 
 
-u32 u64_nibble_count(u64 num)
+u64 u64_nibble_count(u64 num)
 {
-  u32 msb_idx      = bsr64(num | 0b1);
-  u32 nibble_count = 1u + (msb_idx >> 2);
+  u64 msb_idx      = bsr64(num | 0b1);
+  u64 nibble_count = 1ull + (msb_idx >> 2);
   
   return nibble_count;
 }
